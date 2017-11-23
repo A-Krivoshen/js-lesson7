a2838b58e42ebae600b59fa51dc090ebab7017bb
Aleksey Krivoshein <30792444+A-Krivoshen@users.noreply.github.com>
2017-11-23 12:42:58 +0300

    Update json.js
    Исправил.

--- json.js
+++ json.js
@@ -1,7 +1,6 @@
-'user strict';
+'use strict';
 /
-
-//task 1
+//Задание 1
 var positions = [
   {
     title: 'Телепорт бытовой VZHIH-101',
@@ -51,7 +50,7 @@ let result2 = lotCalculator(positions[2], 1);
 
 
 
-// task2
+// задание 2
 
 const deferedPayments = [];
 function deferPay(maker,sumOfShipping,dateOfShipping) {
@@ -75,7 +74,7 @@ deferPay(positions[2], 10, new Date(2030, 6-1, 10));
     console.log(`${deferedPayments[i].paymentDate.toLocaleDateString('ru-Ru')}: ${deferedPayments[i].producer.name}, сумма ${deferedPayments[i].amount} Q`);
   }
   
-// task3
+// Задание 3
 
 function loadCurrencyJSON() {
   return '{"AUD":44.95,"AZN":33.73,"GBP":73.42,"AMD":0.12,"BYN":30.96,"BGN":32.01,"BRL":18.8,"HUF":0.2,"DKK":8.42,"USD":58.85,"EUR":62.68,"INR":0.88,"KZT":0.18,"CAD":44.74,"KGS":0.85,"CNY":8.55,"MDL":2.94,"NOK":7.02,"PLN":14.55,"RON":13.92,"ZZZ":79.91,"SGD":41.36,"TJS":7.43,"TRY":15.97,"TMT":16.84,"UZS":0.02,"UAH":2.16,"CZK":2.32,"SEK":6.6,"CHF":58.69,"ZAR":4.4,"KRW":0.05,"JPY":0.52}';
@@ -100,6 +99,3 @@ let price2 = convertCurrency(790, 'EUR', 'ZZZ');
 console.log(`Сумма ${price2} Q`);
 let price3 = convertCurrency(25, 'AZN', 'CZK');
 console.log(`Сумма ${price3} CZK`);
-
-
-    
